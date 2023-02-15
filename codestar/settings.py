0842d4012095abc0b08a6aaa17d5145d47b9138c
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-02-15 20:45:27 +0000

    Change settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-blog-ak.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['django-blog-ak.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -75,7 +75,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'codestar.wsgi.application'
+WSGI_APPLICATION = 'django-blog-ak.wsgi.application'
 
 
 # Database
